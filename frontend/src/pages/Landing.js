2f020f8dc5df2daaccf7796891f1d454811d57d2
yrik <iurii.kriachko@gmail.com>
2022-05-19 16:35:06 +0800

    1 binaries uploaded

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Header from '../components/Header'
+import Header from '../components/Header';
 
 const stripeStyles = {
   backgroundImage: 'url("/bg.png")',
   minHeight: '265px',
-}
+};
 
 const Banner = () => (
   <div style={stripeStyles} className="text-center">
-    
-    <h1 className="text-3xl pt-20"><b>Vol</b>unteers’ <b>Lia</b>ison — Make it regular</h1>
+    <h1 className="text-3xl pt-20">
+      <b>Vol</b>unteers’ <b>Lia</b>ison — Make it regular
+    </h1>
 
-    <div className="max-w-2xl mt-5 m-auto">Provide volunteers with regular CRYPTO donation. Your donation will be immediately received by volunteer.</div>
-
+    <div className="max-w-2xl mt-5 m-auto">
+      Provide volunteers with regular CRYPTO donation. Your donation will be
+      immediately received by volunteer.
+    </div>
   </div>
-)
+);
 
 const Landing = (props) => {
   return (
