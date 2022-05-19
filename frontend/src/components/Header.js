80ffe6d91fc9e942edb71c0237c7fecb8acd0842
yrik <iurii.kriachko@gmail.com>
2022-05-19 15:39:17 +0800

    2 binaries uploaded

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <div className="navbar bg-white">
       <div class="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl">Volia</a>
+        <a className="btn btn-ghost normal-case text-xl"><img src="/logo.png" /></a>
         
       </div>
       <div className="navbar-end">
