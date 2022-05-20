ea62d0eae2cfa3ecc992c6e5ed4d3f6ae88de8c4
yrik <iurii.kriachko@gmail.com>
2022-05-20 21:44:04 +0800

    2 files modified

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -3,21 +3,50 @@ import Header from '../components/Header';
 import projectsData from '../projects-data';
 import { useParams } from 'react-router-dom';
 
+const Tier = ({ name, amount, currency, period }) => (
+  <div className="card card-normal m-w-60 w-60 bg-base-100 shadow-xl flex-auto max-h-96 m-auto ">
+    <div className="card-body">
+      <h2 className="card-title text-sm font-bold text-center m-auto">
+        {name}
+      </h2>
+      <p className=""></p>
+      <div className="text-center text-neutral mb-2 mt-8">
+        {amount} {currency} / {period}
+      </div>
+      <div className="card-actions justify-center">
+        <button className="btn w-100">Subscribe</button>
+      </div>
+    </div>
+  </div>
+);
+
 const Project = (props) => {
   const { projectID } = useParams();
-  const { title, description } = projectsData[projectID];
+  const { title, description, tiers } = projectsData[projectID];
   return (
     <div>
       <Header />
-      <div
-        className="text-center rounded-lg bg-white h-64 mt-8 m-auto"
-        style={{
-          maxWidth: '1000px',
-          backgroundImage: 'url("/projectBG.png")',
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
-        Project: {title}
+      <div style={{ maxWidth: '1000px' }} className="m-auto">
+        <div
+          className="rounded-lg bg-white h-64 p-12 mt-8 m-auto"
+          style={{
+            backgroundImage: 'url("/projectBG.png")',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'top',
+            paddingTop: '150px',
+          }}
+        >
+          <div className="font-bold text-lg">{title}</div>
+          <div>{description}</div>
+        </div>
+        <h3 className="mt-8 mb-2 text-xl text-center font-bold">
+          Select a membership level
+        </h3>
+        <div className="container-lg mx-12 my-16 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {tiers.map((tier, index) => (
+            <Tier {...tier} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/projects-data.js
+++ frontend/src/projects-data.js
@@ -1,16 +1,40 @@
+const tiers = [
+  {
+    name: 'Supporter Level 1',
+    currency: 'usdc',
+    amount: '200',
+    period: 'month',
+  },
+  {
+    name: 'Supporter Level 2',
+    currency: 'usdc',
+    amount: '400',
+    period: 'month',
+  },
+  {
+    name: 'Supporter Level 3',
+    currency: 'usdc',
+    amount: '600',
+    period: 'month',
+  },
+];
+
 const PROJECTS = [
   {
     title: 'Iryna Mischenko',
+    tiers,
     description:
       'My approach is to announce a very particular fundraise for 1-2 days to buy one thing, for example a military off-road car. Once we did it, I publish a report about a purchase and start a new fundraising. For me, it’s very crucial in terms of fundraising to know soldiers in person or if my friends know them, so we can help real particular people. Secondly, I must be sure that what we bought is really in place and doing its work (shooting, flying etc), and not is being resold on a secondary market. Over 400,000 dollars have been collected in 80 days. ',
   },
   {
     title: 'Denys Koshelnik',
+    tiers,
     description:
       'We’re a team of 10 people, and we use our own cars (5) to help soldiers from Ukrainian army, ​​Territorial Defense Forces with needed equipments.',
   },
   {
     title: 'Anna Pavlovna',
+    tiers,
     description:
       'Our team also covering humanitarian aids, we’re helping children, elderlies and also protecting some ​​animal shelters in Kyiv and the cities nearby.',
   },
