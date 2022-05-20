ce552ab8c9a1f083bf29920db434bbc5f9719877
yrik <iurii.kriachko@gmail.com>
2022-05-20 18:45:52 +0800

    1 file modified

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -3,11 +3,6 @@ import Header from '../components/Header';
 import projectsData from '../projects-data';
 import { useParams } from 'react-router-dom';
 
-const stripeStyles = {
-  backgroundImage: 'url("/projectBG.png")',
-  minHeight: '265px',
-};
-
 const Project = (props) => {
   const { projectID } = useParams();
   const { title, description } = projectsData[projectID];
@@ -18,8 +13,8 @@ const Project = (props) => {
         className="text-center rounded-lg bg-white h-64 mt-8 m-auto"
         style={{
           maxWidth: '1000px',
-          backgroundImage: 'url("/bg2.png")',
-          backgroundPosition: '0px -50px 0px 0px',
+          backgroundImage: 'url("/projectBG.png")',
+          backgroundRepeat: 'no-repeat',
         }}
       >
         Project: {title}
