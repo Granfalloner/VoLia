7f4abe93d88abba9a253a20828846cd16e679cd6
Myronchuk Vasyl <granfalloner@gmail.com>
2022-05-21 19:44:38 +0300

    Add admin page.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Landing from './pages/Landing';
 import Project from './pages/Project';
+import Admin from './pages/Admin';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const App = (props) => {
@@ -9,6 +10,7 @@ const App = (props) => {
       <Routes>
         <Route exact path="/project/:projectId" element={<Project />} />
         <Route exact path="/" element={<Landing />} />
+        <Route exact path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Admin.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import * as ethers from 'ethers';
+import * as Crypto from '../crypto.js';
+import ContractAbi from '../abi/ContractAbi.json';
+
+const ContractAddress = '0xD21b3Ff5798b876C0bD36C1294c2B937cA03C6C1';
+
+const Admin = (props) => {
+  const [wallet, setWallet] = useState(undefined);
+  const [address, setAddress] = useState(undefined);
+
+  const onConnectWallet = async () => {
+    const wallet = await Crypto.connectWallet();
+    setWallet(wallet);
+  };
+
+  useEffect(() => {
+    Crypto.loadWallet();
+    return Crypto.saveWalletOnChange(setWallet);
+  }, []);
+
+  useEffect(() => {
+    if (wallet) {
+      setAddress(wallet.accounts[0].address);
+    }
+  }, [wallet]);
+
+  const registerProject = async () => {
+    const provider = new ethers.providers.Web3Provider(wallet.provider, 'any');
+    const signer = provider.getUncheckedSigner();
+    const contract = new ethers.Contract(ContractAddress, ContractAbi, signer);
+    const tx = await contract.registerProject(address, []);
+    alert(JSON.stringify(tx));
+  }
+  
+  return (
+    <div>
+      <Header wallet={wallet} connectWallet={onConnectWallet} />
+      <div className="text-center">
+        <input type="text" value={address} onChange={e => setAddress(e.value)} placeholder="Type here" className="mt-16 input w-full max-w-xs" />
+        <br />
+        <button className="btn mt-6" onClick={registerProject}>Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
