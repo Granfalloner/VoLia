50200f3d1ecfaf09fd689c0c50e60b2f3e457c7f
yrik <iurii.kriachko@gmail.com>
2022-05-22 11:58:17 +0800

    update contracts

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,11 +1,40 @@
-const utils = require("./utils.js");
+const { ethers, network } = require('hardhat');
+const utils = require('./utils.js');
 
 async function main() {
-  const trustedSubs = await utils.deployContract("TrustedSubscription");
-  console.log("TrustedSubscription deployed to:", trustedSubs.address);
+  const [owner] = await ethers.getSigners();
 
-  await utils.verifyContract(trustedSubs.address);
-  console.log("TrustedSubscription verified");
+  const contract = await utils.deployContract('TrustedSubscription');
+  console.log('TrustedSubscription deployed to:', contract.address);
+
+  if (network.name === 'rinkeby') {
+    const USDCAddress = '0xeb8f08a975Ab53E34D8a0330E0D34de942C95926';
+    await contract.addTokens([USDCAddress]);
+
+    await contract.registerProject(owner.address, [
+      {
+        isActive: true,
+        amount: 200 * 10 ** 6,
+        period: 60 * 60 * 24 * 30,
+        tokenId: 1,
+      },
+      {
+        isActive: true,
+        amount: 400 * 10 ** 6,
+        period: 60 * 60 * 24 * 30,
+        tokenId: 1,
+      },
+      {
+        isActive: true,
+        amount: 600 * 10 ** 6,
+        period: 60 * 60 * 24 * 30,
+        tokenId: 1,
+      },
+    ]);
+  }
+
+  await utils.verifyContract(contract.address);
+  console.log('TrustedSubscription verified');
 }
 
 main()
--- contracts/test/TrustedSubscriptionTests.js
+++ contracts/test/TrustedSubscriptionTests.js
@@ -1,25 +1,71 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const utils = require("../scripts/utils.js");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+const utils = require('../scripts/utils.js');
+const { parseEther } = ethers.utils;
 
-describe("TrustedSubscription", function () {
-
+describe('TrustedSubscription', function () {
   beforeEach(async function () {
     [owner, user] = await ethers.getSigners();
-    token = await utils.deployContract("TestToken");
-    contract = await utils.deployContract("TrustedSubscription");
+    token = await utils.deployContract('TestToken');
+    contract = await utils.deployContract('TrustedSubscription');
     await contract.addTokens([token.address]);
   });
 
-  it("Should work", async function () {
+  it('Should work', async function () {
     const amount = 100;
-    const ethAmount = ethers.utils.parseEther(`${amount}`);
-    await contract.registerProject(owner.address, [{isActive: true, amount: ethAmount, period: 604800, tokenId: 1}])
-    expect((await contract.projects(1)).isActive).to.be.equal(true);
+    const ethAmount = parseEther(`${amount}`);
+    await contract.registerProject(owner.address, [
+      { isActive: true, amount: ethAmount, period: 604800, tokenId: 1 },
+      { isActive: true, amount: ethAmount.mul(2), period: 604800, tokenId: 1 },
+      { isActive: true, amount: ethAmount.mul(3), period: 604800, tokenId: 1 },
+    ]);
+    const projectId = 1;
+    expect((await contract.projects(projectId)).isActive).to.be.equal(true);
 
     await token.mint(user.address, ethAmount.mul(12));
     await token.connect(user).approve(contract.address, ethAmount.mul(12));
-    await contract.connect(user).subscribe(1, 0);
+
+    const tierIndex = 0;
+    const isSubscribedBefore = await contract.isSubscribed(
+      projectId,
+      tierIndex,
+      user.address
+    );
+    expect(isSubscribedBefore).to.be.equal(false);
+    expect(
+      await contract.numberOfSubscribersInTier(projectId, tierIndex)
+    ).to.be.equal(0);
+
+    await contract.connect(user).subscribe(projectId, tierIndex);
+
+    const isSubscribedAfter = await contract.isSubscribed(
+      projectId,
+      tierIndex,
+      user.address
+    );
+    expect(isSubscribedAfter).to.be.equal(true);
+
+    const isSubscribedAfterOtherTier = await contract.isSubscribed(
+      projectId,
+      tierIndex + 1,
+      user.address
+    );
+    expect(isSubscribedAfterOtherTier).to.be.equal(false);
+
+    const isDeployerSubscribed = await contract.isSubscribed(
+      projectId,
+      tierIndex,
+      owner.address
+    );
+    expect(isDeployerSubscribed).to.be.equal(false);
+
+    expect(
+      await contract.numberOfSubscribersInTier(projectId, tierIndex)
+    ).to.be.equal(1);
+    expect(
+      await contract.numberOfSubscribersInTier(projectId, tierIndex + 1)
+    ).to.be.equal(0);
+
     await utils.fastForward(604800);
 
     const claimableAmount = await contract.claimableAmountOfTokens(1, 1);
