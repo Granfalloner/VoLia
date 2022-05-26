441ebb4cd013c9013c5be481a90635a1579ad01f
@Dreamcoder <42254355+AnnaMakota@users.noreply.github.com>
2022-05-26 17:39:47 +0200

    Update projects-data.js

--- frontend/src/projects-data.js
+++ frontend/src/projects-data.js
@@ -1,8 +1,8 @@
 const PROJECTS = [
   {
     projectId: 1,
-    image: '/projects/Iryna Mischenko.jpg',
-    title: 'Iryna Mischenko',
+    image: '/projects/Kolo.jpg',
+    title: 'Kolo',
     address: 'Ukraine',
     ethAddress: '0x261d6B800CBF0cf3F0df6a79ae4C62a70c3b42d1',
     customColor: '#F9DD4B',
@@ -36,11 +36,11 @@ const PROJECTS = [
       },
     ],
     description:
-      'Position: The former owner of the travel agency “Friends”. Currently, I’m a volunteer. I’m helping our military guys with all what they’re asking me for. ​​Description: My approach is to announce a very particular fundraise for 1-2 days to buy one thing, for example a military off-road car. Once we did it, I publish a report about a purchase and start a new fundraising. For me, it’s very crucial in terms of fundraising to know soldiers in person or if my friends know them, so we can help real particular people. Secondly, I must be sure that what we bought is really in place and doing its work (shooting, flying etc), and not is being resold on a secondary market. Over 400,000 dollars have been collected in 80 days. ',
-  },
+      'KOLO is a team of top managers of product companies that have created a fund to provide operational assistance to the Armed Forces. The world is investing much to stop this war. But crimes continue. Technology can accelerate these efforts and prevent worsening of the humanitarian crisis. Help Ukrainians fight the cause of the crisis, not just its consequences.
+'},
   {
     projectId: 2,
-    title: 'Denys Koshelnik',
+    title: 'Alla and Denys Koshelnik',
     address: 'Ukraine',
     ethAddress: '0x261d6B800CBF0cf3F0df6a79ae4C62a70c3b42d1',
     tiers: [
@@ -74,11 +74,11 @@ const PROJECTS = [
     ],
     image: '/projects/Denys Koshelnik.jpg',
     description:
-      'Position: Journalist, blogger, volunteer. ​​Description: We’re a team of 10 people, and we use our own cars (5) to help soldiers from Ukrainian army, ​​Territorial Defense Forces with needed equipments.',
-  },
+      'Description: We’re a team of 10 people, and we use our own cars (5) to help soldiers from Ukrainian army, ​​Territorial Defense Forces with needed equipments.Position: Before the war, I was working in restaurant business, but we were always helping kids and nursing homes. ​​Description: Currently, we’re almost full-time volunteers. ​​ Our team is covering humanitarian aid, we’re helping children, displaced people, ​​ disabled persons, lonely mothers with kids and lonely elderlies.​​ I use individual approach in volunteering there are many people writing to me ​​ asking for the help, many of them are displaced people, ​​so I’m carefully reviewing their requests and try to provide individual special help for each of them, ​​like to select right sizes of clothes or shoes. I try to find the best stuff for them and ​​ I pack it by myself. The only fundraising we have now is for delivery needs. ​​We have to deliver humanitarian aids in Ukraine, and since the price for petrol is dramatically increased,​​ it became harder to transport the aid. Also protecting some ​​animal shelters in Kyiv and the cities nearby. ​​Almost 15% of the humanitarian help we received is for animals, ​​so we are spreading the news once we got it and try to deliver this food to those who need it.',
+  ', },
   {
     projectId: 3,
-    title: 'Alla Pavlovna',
+    title: 'Iryna Mischenko',
     address: 'Ukraine',
     ethAddress: '0x261d6B800CBF0cf3F0df6a79ae4C62a70c3b42d1',
     tiers: [
@@ -110,9 +110,9 @@ const PROJECTS = [
         tierIndex: 2,
       },
     ],
-    image: '/projects/Anna Pavlovna.jpg',
+    image: '/projects/Iryna Mischenko.jpg',
     description:
-      'Position: Before the war, I was working in restaurant business, but we were always helping kids and nursing homes. ​​Description: Currently, we’re almost full-time volunteers. ​​ Our team is covering humanitarian aid, we’re helping children, displaced people, ​​ disabled persons, lonely mothers with kids and lonely elderlies.​​ I use individual approach in volunteering there are many people writing to me ​​ asking for the help, many of them are displaced people, ​​so I’m carefully reviewing their requests and try to provide individual special help for each of them, ​​like to select right sizes of clothes or shoes. I try to find the best stuff for them and ​​ I pack it by myself. The only fundraising we have now is for delivery needs. ​​We have to deliver humanitarian aids in Ukraine, and since the price for petrol is dramatically increased,​​ it became harder to transport the aid. Also protecting some ​​animal shelters in Kyiv and the cities nearby. ​​Almost 15% of the humanitarian help we received is for animals, ​​so we are spreading the news once we got it and try to deliver this food to those who need it.',
+      'Position: The former owner of the travel agency “Friends”. Currently, I’m a volunteer. I’m helping our military guys with all what they’re asking me for. ​​Description: My approach is to announce a very particular fundraise for 1-2 days to buy one thing, for example a military off-road car. Once we did it, I publish a report about a purchase and start a new fundraising. For me, it’s very crucial in terms of fundraising to know soldiers in person or if my friends know them, so we can help real particular people. Secondly, I must be sure that what we bought is really in place and doing its work (shooting, flying etc), and not is being resold on a secondary market. Over 400,000 dollars have been collected in 80 days. ',
   },
 ];
 
