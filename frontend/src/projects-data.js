2895c0286dc1d60f77d2ec19f0781c2a573af773
@Dreamcoder <42254355+AnnaMakota@users.noreply.github.com>
2022-05-27 16:35:10 +0200

    Update projects-data.js

--- frontend/src/projects-data.js
+++ frontend/src/projects-data.js
@@ -47,7 +47,7 @@ const PROJECTS = [
       {
         name: 'Crypto Peasant',
         currency: 'usdc',
-        amount: 50,
+        amount: 10,
         period: 'week',
         tokenId: 1,
         tokenDecimals: 6,
@@ -56,7 +56,7 @@ const PROJECTS = [
       {
         name: 'Crypto Warrior',
         currency: 'usdc',
-        amount: 100,
+        amount: 50,
         period: '2 weeks',
         tokenId: 1,
         tokenDecimals: 6,
@@ -65,7 +65,7 @@ const PROJECTS = [
       {
         name: 'Crypto Lord',
         currency: 'usdc',
-        amount: 500,
+        amount: 150,
         period: 'month',
         tokenId: 1,
         tokenDecimals: 6,
@@ -74,7 +74,7 @@ const PROJECTS = [
     ],
     image: '/projects/All and Denys.jpg',
     description:
-      'Description: We’re a team of 10 people, and we use our own cars (5) to help soldiers from Ukrainian army, ​​Territorial Defense Forces with needed equipments.Position: Before the war, I was working in restaurant business, but we were always helping kids and nursing homes. ​​Description: Currently, we’re almost full-time volunteers. ​​ Our team is covering humanitarian aid, we’re helping children, displaced people, ​​ disabled persons, lonely mothers with kids and lonely elderlies.​​ I use individual approach in volunteering there are many people writing to me ​​ asking for the help, many of them are displaced people, ​​so I’m carefully reviewing their requests and try to provide individual special help for each of them, ​​like to select right sizes of clothes or shoes. I try to find the best stuff for them and ​​ I pack it by myself. The only fundraising we have now is for delivery needs. ​​We have to deliver humanitarian aids in Ukraine, and since the price for petrol is dramatically increased,​​ it became harder to transport the aid. Also protecting some ​​animal shelters in Kyiv and the cities nearby. ​​Almost 15% of the humanitarian help we received is for animals, ​​so we are spreading the news once we got it and try to deliver this food to those who need it.', 
+      'We’re a team of 10 people, and we use our own cars (5) to help soldiers from Ukrainian army, ​​Territorial Defense Forces with needed equipments. Before the war, we were working in restaurant business, but we were always helping kids and nursing homes. ​​Currently, we’re almost full-time volunteers. ​​ Our team is covering humanitarian aid, we’re helping children, displaced people, ​​ disabled persons, lonely mothers with kids and lonely elderlies.​​ There are many people writing to us ​​ asking for the help, many of them are displaced people, ​​so we are carefully reviewing their requests and try to provide individual special help for each of them, ​​like to select right sizes of clothes or shoes. We try to find the best stuff for them and. ​​ The only fundraising we have now is for delivery needs. ​​We have to deliver humanitarian aids in Ukraine, and since the price for petrol is dramatically increased,​​ it became harder to transport the aid. Also we are protecting some ​​animal shelters in Kyiv and the cities nearby. ​​Almost 15% of the humanitarian help we received is for animals, ​​so we are spreading the news once we got it and try to deliver this food to those who need it.', 
 },
   {
     projectId: 3,
@@ -85,7 +85,7 @@ const PROJECTS = [
       {
         name: 'Crypto Peasant',
         currency: 'usdc',
-        amount: 50,
+        amount: 10,
         period: 'week',
         tokenId: 1,
         tokenDecimals: 6,
@@ -94,7 +94,7 @@ const PROJECTS = [
       {
         name: 'Crypto Warrior',
         currency: 'usdc',
-        amount: 100,
+        amount: 50,
         period: '2 weeks',
         tokenId: 1,
         tokenDecimals: 6,
@@ -103,7 +103,7 @@ const PROJECTS = [
       {
         name: 'Crypto Lord',
         currency: 'usdc',
-        amount: 500,
+        amount: 200,
         period: 'month',
         tokenId: 1,
         tokenDecimals: 6,
@@ -112,7 +112,7 @@ const PROJECTS = [
     ],
     image: '/projects/Iryna Mischenko.jpg',
     description:
-      'Position: The former owner of the travel agency “Friends”. Currently, I’m a volunteer. I’m helping our military guys with all what they’re asking me for. ​​Description: My approach is to announce a very particular fundraise for 1-2 days to buy one thing, for example a military off-road car. Once we did it, I publish a report about a purchase and start a new fundraising. For me, it’s very crucial in terms of fundraising to know soldiers in person or if my friends know them, so we can help real particular people. Secondly, I must be sure that what we bought is really in place and doing its work (shooting, flying etc), and not is being resold on a secondary market. Over 400,000 dollars have been collected in 80 days. ',
+      'I am the former owner of the travel agency “Friends”. Currently, I’m a volunteer. I’m helping our military guys with all what they’re asking me for. ​​ My approach is to announce a very particular fundraise for 1-2 days to buy one thing, for example a military off-road car. Once we did it, I publish a report about a purchase and start a new fundraising. For me, it’s very crucial in terms of fundraising to know soldiers in person or if my friends know them, so we can help real particular people. Secondly, I must be sure that what we bought is really in place and doing its work (shooting, flying etc), and not is being resold on a secondary market. Over 400,000 dollars have been collected in 80 days. ',
   },
 ];
 
