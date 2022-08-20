205e180fc943d39624af0b8bfecba958d46a8f9b
yrik <iurii.kriachko@gmail.com>
2022-08-20 22:10:15 +0200

    update admin page

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -4,10 +4,15 @@ import * as ethers from 'ethers';
 import * as Crypto from '../crypto.js';
 import ContractAbi from '../abi/ContractAbi.json';
 import { config } from '../config'
+const { parseUnits, formatUnits } = ethers.utils;
 
 const Admin = (props) => {
   const [wallet, setWallet] = useState(undefined);
-  const [address, setAddress] = useState(undefined);
+  const [address, setAddress] = useState('');
+
+  const [price1, setPrice1] = useState(50);
+  const [price2, setPrice2] = useState(200);
+  const [price3, setPrice3] = useState(500);
 
   const onConnectWallet = async () => {
     const wallet = await Crypto.connectWallet();
@@ -30,16 +35,46 @@ const Admin = (props) => {
     const signer = provider.getUncheckedSigner();
     const { USDCAddress, ContractAddress } = config[wallet.chains[0].id];
     const contract = new ethers.Contract(ContractAddress, ContractAbi, signer);
-    const tx = await contract.registerProject(address, []);
-    alert(JSON.stringify(tx));
-  }
+    const decimals = 6 // all except mumbai (18)
+    const tiers = [
+      {
+        isActive: true,
+        amount: parseUnits(`${price1}`, decimals),
+        period: 60 * 60 * 24 * 30,
+      },
+      {
+        isActive: true,
+        amount: parseUnits(`${price2}`, decimals),
+        period: 60 * 60 * 24 * 30,
+      },
+      {
+        isActive: true,
+        amount: parseUnits(`${price3}`, decimals),
+        period: 60 * 60 * 24 * 30,
+      },
+
+    ]
+    console.log(address, USDCAddress, tiers)
+    try {
+      const tx = await contract.registerProject(address, USDCAddress, tiers);
+      alert(JSON.stringify(tx));
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+ }
   
   return (
     <div>
       <Header wallet={wallet} connectWallet={onConnectWallet} />
       <div className="text-center">
         <label htmlFor="address">Receiver of funds:</label>
-        <input id="address" type="text" value={address} onChange={e => setAddress(e.value)} placeholder="Type here" className="mt-16 input w-full max-w-xs" />
+        <input id="address" type="text" value={address} onChange={e => setAddress(e.value)} placeholder="Type here" className="mt-16 mb-5 input w-full max-w-xs" />
+        <br />
+        monthly tiers:
+        <br />
+        <input type="number" value={price1} onChange={e => {setPrice1(e.value)}} placeholder="Price 1" className="input " />
+        <input type="number" value={price2} onChange={e => {setPrice2(e.value)}} placeholder="Price 2" className="input " />
+        <input type="number" value={price3} onChange={e => {setPrice3(e.value)}} placeholder="Price 3" className="input " />
         <br />
         <button className="btn mt-6" onClick={registerProject}>Create Project</button>
       </div>
